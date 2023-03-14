5d848e3e538a204c9684104dd2729020b6e86636
Julien FRANCOIS <Julien.FRANCOIS@hardis-group.com>
2023-03-14 15:28:10 +0100

    control form

--- my-app/src/interfaces/weather.ts
+++ my-app/src/interfaces/weather.ts
@@ -2,3 +2,9 @@ export interface weatherCoords {
   lat: number;
   long: number;
 }
+
+export interface addWeather {
+  name: string;
+  lat: number;
+  long: number;
+}
--- my-app/src/pages/Form.tsx
+++ my-app/src/pages/Form.tsx
@@ -1,14 +1,72 @@
-import React from "react";
+import React, { SyntheticEvent, useState } from "react";
 
 const Form = () => {
+  const [name, setName] = useState<string>();
+  const [long, setLong] = useState<string>();
+  const [lat, setLat] = useState<string>();
+  const [error, setError] = useState<string>();
+  const [success, setSuccess] = useState<string>();
+
+  const onSubmit = (event: SyntheticEvent) => {
+    event.preventDefault();
+
+    // !name ?? setError('Veuillez rentrer le nom de la ville');
+    if (!name) {
+      setError("Veuillez rentrer le nom de la ville");
+      setSuccess("");
+      return;
+    }
+
+    if (!long) {
+      setError("Veuillez rentrer la longitude");
+      setSuccess("");
+      return;
+    }
+
+    if (!lat) {
+      setError("Veuillez rentrer la latitude");
+      setSuccess("");
+      return;
+    }
+
+    setError("");
+    setSuccess("Formulaire envoyé");
+    console.log(name, long, lat);
+  };
+
+  error !== "" ?? (
+    <div className="form-err" role="alert">
+      {error}
+    </div>
+  );
+
+  error !== "" ?? (
+    <div className="form-win" role="alert">
+      {success}
+    </div>
+  );
+
   return (
     <div>
       <form className="container-form">
+        {error}
+        {success}
+        <div className="form-control">
+          <input
+            type="text"
+            className="form-input"
+            placeholder="Ville..."
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
         <div className="form-control">
           <input
             type="text"
             className="form-input"
             placeholder="Longitude..."
+            value={long}
+            onChange={(e) => setLong(e.target.value)}
           />
         </div>
         <div className="form-control">
@@ -16,9 +74,11 @@ const Form = () => {
             type="text"
             className="form-input"
             placeholder="Latitude..."
+            value={lat}
+            onChange={(e) => setLat(e.target.value)}
           />
         </div>
-        <button type="submit" className="btn-home">
+        <button type="submit" className="btn-home" onClick={onSubmit}>
           Envoyer
         </button>
       </form>
